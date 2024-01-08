cfc762d4eccdb848eaa5a4898266086e8d3a0020
wipesides <94230409+wipesides@users.noreply.github.com>
2024-01-08 20:52:20 +0300

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
-export const API_KEY = 'd313bd90-0d11-4fe9-8b22-9dba5a2113f9';
+export const API_KEY = 'YOUR API KEY';
 export const RESULTS_PER_PAGE = 10;
 export const MODEL_CLOSE_SECONDS = 2.5;
